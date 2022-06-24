375ecd9e5276e0e796d5f0b803859774fc3861c4
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-06-24 16:24:12 +0200

    Add files via upload

---
+++ src/id_details_extractor/ID_Details_Extractor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package id_details_extractor;
+
+/**
+ *
+ * @author Shady Emad
+ */
+public class ID_Details_Extractor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ID_Extractor().setVisible(true);
+    }
+    
+}
